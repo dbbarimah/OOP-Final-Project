998bc27de830bb9561f0c4eede5bcc520d4e77f8
dbbarimah <dbbarimah@gmail.com>
2025-12-02 13:29:50 +0000

    Implement report management in ReportGenerator
    Added methods to manage weekly reports.

--- ReportGenerator.java
+++ ReportGenerator.java
@@ -1,5 +1,13 @@
 import java.util.HashMap;
 
 public class ReportGenerator{
-    
+    private HashMap<Integer, WeeklyReport> allReports = new HashMap<>();
+
+    public void addReport(WeeklyReport report){
+        report.put(Report.getWeekNumber(), report);
+    }
+
+    public void generateReport(int weekNumber){
+        
+    }
 }
