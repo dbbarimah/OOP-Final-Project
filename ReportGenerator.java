e7b93035a747aca28c69cba75b6649636a96a3bd
dbbarimah <dbbarimah@gmail.com>
2025-12-02 13:36:10 +0000

    Edited ReportGenerator

--- ReportGenerator.java
+++ ReportGenerator.java
@@ -4,10 +4,14 @@ public class ReportGenerator{
     private HashMap<Integer, WeeklyReport> allReports = new HashMap<>();
 
     public void addReport(WeeklyReport report){
-        report.put(Report.getWeekNumber(), report);
+        allReports.put(report.getWeekNumber(), report);
     }
 
     public void generateReport(int weekNumber){
-        
+        if (allReports.containsKey(weekNumber)){
+            report.get(weekNumber).printFinalReport();
+        } else {
+            System.out.println("No report found for Week: " + weekNumber);
+        }
     }
 }
