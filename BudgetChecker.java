7ebb81eac571983a7aa81e0e9f81dc5f979def8c
dbbarimah <dbbarimah@gmail.com>
2025-12-02 10:18:03 +0000

    Implement ReportGenerator class for budget reporting
    Added ReportGenerator class to handle weekly budget reports with member variables and methods for report generation.

--- BudgetChecker.java
+++ BudgetChecker.java
@@ -1,3 +1,47 @@
-public class BudgetChecker {
-    
+public class ReportGenerator {
+    // Member Variables For The Report Generator
+    private int weekNumber;
+    private double food;
+    private double vehicle;
+    private double water;
+    private double waste;
+    private double electricity;
+    private double total;
+
+    // Parametised Constructor For The Report Generator
+    public ReportGenerator(
+        int weekNumber,
+        double food,
+        double vehicle,
+        double water,
+        double waste,
+        double electricity,
+        double total
+    ){
+        this.weekNumber = weekNumber;
+        this.food = food;
+        this.vehicle = vehicle;
+        this.water = water;
+        this.waste = waste;
+        this.electricity = electricity;
+        this.total = total;
+    }
+
+    //Essential Getters To Retrieve the Report For A Specified Week
+    public int getWeekNumber(){
+        return weekNumber;
+    }
+    public double getTotal(){
+        return total;
+    }
+
+    //Method To Print Out The Final Report Of The User
+    public void printFinalReport(){
+        System.out.println("This is your report for Week " + weekNumber + "\n" +
+        "Food Emissions: " + food + "\n" +
+        "Vehicle Emissions: " + vehicle + "\n" +
+        "Water Emissions: " + water + "\n" + 
+        "Waste Emissions: " + waste + "\n" +
+        "Electricity Emissions: " + electricity);
+    }
 }
